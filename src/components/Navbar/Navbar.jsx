d032f915df4253dbfb4904fd166efa87979f71c5
fmfahath <fmfahath.dev@gmail.com>
2024-03-19 14:11:00 +0530

    navbar scroll - bg color change effect added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+
+    const [darkNav, setDarkNav] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setDarkNav(true) : setDarkNav(false)
+        })
+    }, [])
+
     return (
-        <nav className='container'>
+        <nav className={`container ${darkNav ? 'dark-nav' : ''}`}>
             <img src={logo} alt='' className='logo' />
             <ul>
                 <li>Home</li>
