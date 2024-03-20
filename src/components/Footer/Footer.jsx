ad7b743de7edeca9af8f7a6e45216c2581197de5
fmfahath <fmfahath.dev@gmail.com>
2024-03-20 14:02:55 +0530

    footer component added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>© 2024 Edusity. All rights reserved</p>
+            <ul>
+                <li>Privacy policies</li>
+                <li>Return policies</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
