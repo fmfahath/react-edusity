d276338dad4ad819bba08c7ebac6e211ae0c5613
fmfahath <fmfahath.dev@gmail.com>
2024-03-22 00:22:49 +0530

    nav menu link select : show/hide function added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,12 +25,12 @@ const Navbar = () => {
         <nav className={`container ${darkNav ? 'dark-nav' : ''} `}>
             <Link to='hero' smooth={true} offset={0} duration={500} ><img src={logo} alt='' className='logo' /></Link>
             <ul className={mobileView ? '' : 'hideMenu'}>
-                <li><Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></li>
-                <li><Link to='program' smooth={true} offset={-260} duration={500} >Program</Link></li>
-                <li><Link to='about' smooth={true} offset={-150} duration={500} >About Us</Link></li>
-                <li><Link to='campus' smooth={true} offset={-260} duration={500} >Campus</Link></li>
-                <li><Link to='testimonials' smooth={true} offset={-260} duration={500} >Testimonials</Link></li>
-                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
+                <li><Link to='hero' smooth={true} offset={0} duration={500} onClick={() => setMobileView(false)}>Home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500} onClick={() => setMobileView(false)}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500} onClick={() => setMobileView(false)}>About Us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500} onClick={() => setMobileView(false)}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500} onClick={() => setMobileView(false)}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn' onClick={() => setMobileView(false)}>Contact Us</Link></li>
             </ul>
             <img src={menu_icon} className='menu-icon' onClick={toggleMenu} />
         </nav >
